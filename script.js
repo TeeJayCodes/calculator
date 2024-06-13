f62559bf8867a38f876fb2c9a29a8fe049da2b3c
TJ <TimothyJCar@gmail.com>
2024-06-12 23:40:02 -0400

    Add calculator logic
    This ensures the calculator functions as expected

--- script.js
+++ script.js
@@ -16,63 +16,183 @@ function divide(firstNumber, secondNumber) {
 }
 
 function operate(operator, firstNumber, secondNumber) {
-    return operator(firstNumber, secondNumber);
+    switch (operator) {
+        case 'add':
+            return add(firstNumber, secondNumber);
+        case 'subtract':
+            return subtract(firstNumber, secondNumber);
+        case 'multiply':
+            return multiply(firstNumber, secondNumber);
+        case 'divide':
+            return divide(firstNumber, secondNumber);
+    }
 }
 
 //enable and disable buttons
 function disableButton(button) {
-    button.style.opacity = "0.33";
+    button.classList.add("disableButton");
+    button.classList.remove("enableButton");
 }
 
 function enableButton(button) {
-    button.style.opacity = "1";
+    button.classList.add("enableButton");
+    button.classList.remove("disableButton");
+}
+
+//highlight selected operator button
+function highlightButton(button) {
+    button.classList.add("highlightButton");
+}
+
+function removeHighlight(button) {
+    button.classList.remove("highlightButton");
+}
+
+//check if any operator button is highlighted
+function checkForActiveOperator() {
+    return operatorActive = PLUS.classList.contains("highlightButton") || MINUS.classList.contains("highlightButton") || MULTIPLY.classList.contains("highlightButton") || DIVIDE.classList.contains("highlightButton");
+}
+
+//clear the display
+function clearDisplay() {
+    displayValue.textContent = "";
 }
 
 //enable and disable buttons based on the display value
-function checkForValue() {
+function disableOrEnableCBDButtons() {
+    //display is empty
     if(displayValue.textContent == "") {
         disableButton(CLEAR);
         disableButton(BACKSPACE);
         enableButton(DECIMAL);
-    } else if ((displayValue.textContent.includes("."))) {
+    //decimal has already been used while inputting the first number
+    } else if ((displayValue.textContent.includes(".")) && operatorActive == false ) {
         enableButton(CLEAR);
         enableButton(BACKSPACE);
         disableButton(DECIMAL);
-    } else if (!(displayValue.textContent.includes(".") && displayValue.textContent == "")) {
+    //decimal has already been used while inputting the first number and an operator is selected
+    } else if (displayValue.textContent.includes(".") && (secondNumber === "") && operatorActive == true) {
         enableButton(CLEAR);
         enableButton(BACKSPACE);
         enableButton(DECIMAL);
+    //display is not blank and does not contain a decimal
+    } else if (!(displayValue.textContent.includes(".")) && !(displayValue.textContent == "")) {
+        enableButton(CLEAR);
+        enableButton(BACKSPACE);
+        enableButton(DECIMAL);
+    } else if (displayValue.textContent.includes(".") && (!(secondNumber === "")) && (operatorActive == true)) {
+        enableButton(CLEAR);
+        enableButton(BACKSPACE);
+        disableButton(DECIMAL);
     }
 }
 
-
 //update the display based on the button clicked
 function updateDisplay() {
-    checkForValue();
+    disableOrEnableCBDButtons();
     numButton.forEach(button => button.addEventListener("click", function() {
-        displayValue.textContent += button.textContent;
-        checkForValue();
-    }));
-    DECIMAL.addEventListener("click", function() {
-        if(!(displayValue.textContent.includes("."))) {
-            displayValue.textContent += ".";
-            checkForValue();
+        checkForActiveOperator();
+        if (operatorActive == false) {
+            displayValue.textContent += button.textContent;
+            firstNumber = Number(displayValue.textContent);
+        } else if ((operatorActive == true) && (secondNumber === "") ) {
+            displayValue.textContent = button.textContent;
+            secondNumber = Number(displayValue.textContent);
+        } else if ((operatorActive == true) && (!(secondNumber === "")) ) {
+            displayValue.textContent += button.textContent;
+            secondNumber = Number(displayValue.textContent);
         }
+        disableOrEnableCBDButtons();
+    }));
+    operatorButton.forEach(button => button.addEventListener("click", function() {
+        //evaluate current expression if there is one, before changing operators
+        if (operatorActive) {
+            displayValue.textContent = operate(currentOperator, firstNumber, secondNumber);
+            if (displayValue.textContent.includes(".")) {
+                displayValue.textContent = Math.round((Number(displayValue.textContent) + Number.EPSILON) * 100000) / 100000;
+            } 
+            firstNumber = Number(displayValue.textContent);
+            secondNumber = "";
+        }
+
+        switch (button.textContent) {
+            case '+':
+                currentOperator = 'add';
+                break;
+            case '-':
+                currentOperator = 'subtract';
+                break;
+            case '*':
+                currentOperator = 'multiply';
+                break;
+            case '÷':
+                currentOperator = 'divide';
+                break;
+        }
+        operatorButton.forEach(button => removeHighlight(button));
+        highlightButton(button);
+        checkForActiveOperator();
+        disableOrEnableCBDButtons();
+    }));
+    EQUALS.addEventListener("click", function() {
+        if (secondNumber == "") {
+            //do nothing
+        } else {
+            operatorButton.forEach(button => removeHighlight(button));
+            displayValue.textContent = operate(currentOperator, firstNumber, secondNumber);
+            firstNumber = Number(displayValue.textContent);
+            secondNumber = "";
+            checkForActiveOperator();
+            //limit decimal to 5 places
+            if (displayValue.textContent.includes(".")) {
+                displayValue.textContent = Math.round((Number(displayValue.textContent) + Number.EPSILON) * 100000) / 100000;
+                firstNumber = Number(displayValue.textContent);
+            } 
+        }
+        disableOrEnableCBDButtons();
+    })
+    DECIMAL.addEventListener("click", function() {
+        checkForActiveOperator();
+        if ((displayValue.textContent.includes(".")) ) {
+            //do nothing
+        } else {
+            if (operatorActive == false) {
+                displayValue.textContent += DECIMAL.textContent;
+                firstNumber = Number(displayValue.textContent); 
+            } else if ((operatorActive == true) && (secondNumber === "") ) {
+                displayValue.textContent = DECIMAL.textContent;
+                secondNumber = Number(displayValue.textContent);
+            } else if ((operatorActive == true) && (!(secondNumber === "")) ) {
+                displayValue.textContent += DECIMAL.textContent;
+                secondNumber = Number(displayValue.textContent);
+            }
+        }
+        disableOrEnableCBDButtons();
     });
     CLEAR.addEventListener("click", function() {
-        displayValue.textContent = "";
-        checkForValue();
+        clearDisplay();
+        firstNumber = "";
+        secondNumber = "";
+        currentOperator = "";
+        operatorButton.forEach(button => removeHighlight(button));
+        checkForActiveOperator()
+        disableOrEnableCBDButtons();
     });
     BACKSPACE.addEventListener("click", function() {
-        displayValue.textContent = displayValue.textContent.substring(0, displayValue.textContent.length - 1);
-        checkForValue();
+        displayValue.textContent = displayValue.textContent.substring(0, displayValue.textContent.length - 1); 
+        disableOrEnableCBDButtons();
     });
 }
 
 
+
 //variables used to populate the display when a button is clicked
+let firstNumber = "";
+let secondNumber = "";
+let currentOperator = "";
 let displayValue = document.querySelector(".display > p");
 let numButton = document.querySelectorAll(".numButton");
+let operatorButton = document.querySelectorAll(".operatorButton");
 const ZERO = document.querySelector("#zero");
 const DECIMAL = document.querySelector("#decimal");
 const EQUALS = document.querySelector("#equals");
@@ -91,8 +211,7 @@ const NINE = document.querySelector("#nine");
 const DIVIDE = document.querySelector("#divide");
 const CLEAR = document.querySelector("#clear");
 const BACKSPACE = document.querySelector("#backspace");
-
+let operatorActive = false;
 
 //run updateDisplay function when page loads
 updateDisplay();
-
